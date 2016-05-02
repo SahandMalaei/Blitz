dff009363fd0f338de091658a3a478728acd5468
Sahand Malaei Balasi <sahandmalaei@gmail.com>
2016-05-02 14:35:48 +0100

    Added "Blitz_BaseTypes.h" file.

---
+++ Source/Blitz_BaseTypes.h
@@ -0,0 +1,17 @@
+#ifndef BLITZ_BASETYPES_H
+#define BLITZ_BASETYPES_H 1
+
+#include <stdint.h>
+
+namespace blitz
+{
+	typedef int8_t Int8;
+	typedef uint8_t Uint8;
+	typedef int16_t Int16;
+	typedef uint16_t Uint16;
+	typedef int32_t Int32;
+	typedef uint32_t Uint32;
+	typedef int8_t Bool;
+}
+
+#endif
