381867107294be23468eda9cda9e60e7077721f4
Sahand Malaei Balasi <sahandmalaei@gmail.com>
2016-05-11 20:51:21 +0430

    Added the "Vec3" class.
    Also fixed some naming problems.

--- Source/Blitz_Math.cpp
+++ Source/Blitz_Math.cpp
@@ -51,11 +51,11 @@ namespace blitz
 			vector0.y *= vector1.y;
 			return vector0;
 		}
-		Vec2 & operator *=(Vec2 &vector0, float number)
+		Vec2 & operator *=(Vec2 &vector, float number)
 		{
-			vector0.x *= number;
-			vector0.y *= number;
-			return vector0;
+			vector.x *= number;
+			vector.y *= number;
+			return vector;
 		}
 		Vec2 & operator /=(Vec2 &vector0, const Vec2 &vector1)
 		{
@@ -63,11 +63,11 @@ namespace blitz
 			vector0.y /= vector1.y;
 			return vector0;
 		}
-		Vec2 & operator /=(Vec2 &vector0, float number)
+		Vec2 & operator /=(Vec2 &vector, float number)
 		{
-			vector0.x /= number;
-			vector0.y /= number;
-			return vector0;
+			vector.x /= number;
+			vector.y /= number;
+			return vector;
 		}
 		const Vec2 operator +(const Vec2 &vector0, const Vec2 &vector1)
 		{
@@ -97,6 +97,113 @@ namespace blitz
 		float distance(const Vec2 &position0, const Vec2 &position1)
 		{
 			return Vec2(position1 - position0).length();
+		}
+		/* --------------------------------------------------------------------------- */
+		Vec3::Vec3() :
+			x(0.0f),
+			y(0.0f),
+			z(0.0f)
+		{
+
+		}
+		Vec3::Vec3(const Vec3 &vector) :
+			x(vector.x),
+			y(vector.y),
+			z(vector.z)
+		{
+		}
+		Vec3::Vec3(float x, float y, float z) :
+			x(x),
+			y(y),
+			z(y)
+		{
+		}
+		float Vec3::length() const
+		{
+			return sqrtf(x * x + y * y + z * z);
+		}
+		Vec3 & Vec3::operator =(const Vec3 &vector)
+		{
+			x = vector.x;
+			y = vector.y;
+			z = vector.z;
+			return *this;
+		}
+		/* --------------------------------------------------------------------------- */
+		Vec3 & operator +=(Vec3 &vector0, const Vec3 &vector1)
+		{
+			vector0.x += vector1.x;
+			vector0.y += vector1.y;
+			vector0.z += vector1.z;
+			return vector0;
+		}
+		Vec3 & operator -=(Vec3 &vector0, const Vec3 &vector1)
+		{
+			vector0.x -= vector1.x;
+			vector0.y -= vector1.y;
+			vector0.z -= vector1.z;
+			return vector0;
+		}
+		Vec3 & operator *=(Vec3 &vector0, const Vec3 &vector1)
+		{
+			vector0.x *= vector1.x;
+			vector0.y *= vector1.y;
+			vector0.z *= vector1.z;
+			return vector0;
+		}
+		Vec3 & operator *=(Vec3 &vector, float number)
+		{
+			vector.x *= number;
+			vector.y *= number;
+			vector.z *= number;
+			return vector;
+		}
+		Vec3 & operator /=(Vec3 &vector0, const Vec3 &vector1)
+		{
+			vector0.x /= vector1.x;
+			vector0.y /= vector1.y;
+			vector0.z /= vector1.z;
+			return vector0;
+		}
+		Vec3 & operator /=(Vec3 &vector, float number)
+		{
+			vector.x /= number;
+			vector.y /= number;
+			vector.z /= number;
+			return vector;
+		}
+		const Vec3 operator +(const Vec3 &vector0, const Vec3 &vector1)
+		{
+			return Vec3(vector0.x + vector1.x, vector0.y + vector1.y,
+				vector0.z + vector1.z);
+		}
+		const Vec3 operator -(const Vec3 &vector0, const Vec3 &vector1)
+		{
+			return Vec3(vector0.x - vector1.x, vector0.y - vector1.y,
+				vector0.z - vector1.z);
+		}
+		const Vec3 operator *(const Vec3 &vector0, const Vec3 &vector1)
+		{
+			return Vec3(vector0.x * vector1.x, vector0.y * vector1.y,
+				vector0.z * vector1.z);
+		}
+		const Vec3 operator *(const Vec3 &vector, float number)
+		{
+			return Vec3(vector.x * number, vector.y * number, vector.z * number);
+		}
+		const Vec3 operator /(const Vec3 &vector0, const Vec3 &vector1)
+		{
+			return Vec3(vector0.x / vector1.x, vector0.y / vector1.y,
+				vector0.z / vector1.z);
+		}
+		const Vec3 operator /(const Vec3 &vector, float number)
+		{
+			return Vec3(vector.x / number, vector.y / number, vector.z / number);
+		}
+		/* --------------------------------------------------------------------------- */
+		float distance(const Vec3 &position0, const Vec3 &position1)
+		{
+			return Vec3(position1 - position0).length();
 		}
 	}
 }
--- Source/Blitz_Math.h
+++ Source/Blitz_Math.h
@@ -23,9 +23,9 @@ namespace blitz
 		Vec2 & operator +=(Vec2 &vector0, const Vec2 &vector1);
 		Vec2 & operator -=(Vec2 &vector0, const Vec2 &vector1);
 		Vec2 & operator *=(Vec2 &vector0, const Vec2 &vector1);
-		Vec2 & operator *=(Vec2 &vector0, float number);
+		Vec2 & operator *=(Vec2 &vector, float number);
 		Vec2 & operator /=(Vec2 &vector0, const Vec2 &vector1);
-		Vec2 & operator /=(Vec2 &vector0, float number);
+		Vec2 & operator /=(Vec2 &vector, float number);
 		const Vec2 operator +(const Vec2 &vector0, const Vec2 &vector1);
 		const Vec2 operator -(const Vec2 &vector0, const Vec2 &vector1);
 		const Vec2 operator *(const Vec2 &vector0, const Vec2 &vector1);
@@ -34,6 +34,34 @@ namespace blitz
 		const Vec2 operator /(const Vec2 &vector, float number);
 
 		float distance(const Vec2 &position0, const Vec2 &position1);
+
+		struct Vec3
+		{
+		public:
+			float x, y, z;
+
+			Vec3();
+			Vec3(const Vec3 &vector);
+			Vec3(float x, float y, float z);
+			Vec3 & operator =(const Vec3 &vector);
+
+			float length() const;
+		};
+
+		Vec3 & operator +=(Vec3 &vector0, const Vec3 &vector1);
+		Vec3 & operator -=(Vec3 &vector0, const Vec3 &vector1);
+		Vec3 & operator *=(Vec3 &vector0, const Vec3 &vector1);
+		Vec3 & operator *=(Vec3 &vector, float number);
+		Vec3 & operator /=(Vec3 &vector0, const Vec3 &vector1);
+		Vec3 & operator /=(Vec3 &vector, float number);
+		const Vec3 operator +(const Vec3 &vector0, const Vec3 &vector1);
+		const Vec3 operator -(const Vec3 &vector0, const Vec3 &vector1);
+		const Vec3 operator *(const Vec3 &vector0, const Vec3 &vector1);
+		const Vec3 operator *(const Vec3 &vector, float number);
+		const Vec3 operator /(const Vec3 &vector0, const Vec3 &vector1);
+		const Vec3 operator /(const Vec3 &vector, float number);
+
+		float distance(const Vec3 &position0, const Vec3 &position1);
 	}
 }
 
