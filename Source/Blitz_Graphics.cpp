d95c9470ddeab37ac5674e59a4e748da445ba257
Sahand Malaei Balasi <sahandmalaei@gmail.com>
2016-05-15 20:33:02 +0100

    Added structs "ColorRgb", "ColorRgba", and "Vertex".

--- Source/Blitz_Graphics.cpp
+++ Source/Blitz_Graphics.cpp
@@ -4,5 +4,84 @@ namespace blitz
 {
 	namespace graphics
 	{
+		ColorRgb::ColorRgb() :
+			r(0.0f),
+			g(0.0f),
+			b(0.0f)
+		{
+		}
+		ColorRgb::ColorRgb(const ColorRgb &color) :
+			r(color.r),
+			g(color.g),
+			b(color.b)
+		{
+		}
+		ColorRgb::ColorRgb(float r, float g, float b) :
+			r(r),
+			g(g),
+			b(b)
+		{
+		}
+		ColorRgb & ColorRgb::operator =(const ColorRgb &color)
+		{
+			r = color.r;
+			g = color.g;
+			b = color.b;
+			return *this;
+		}
+		/* --------------------------------------------------------------------------- */
+		ColorRgba::ColorRgba() :
+			r(0.0f),
+			g(0.0f),
+			b(0.0f),
+			a(0.0f)
+		{
+		}
+		ColorRgba::ColorRgba(const ColorRgba &color) :
+			r(color.r),
+			g(color.g),
+			b(color.b),
+			a(color.a)
+		{
+		}
+		ColorRgba::ColorRgba(float r, float g, float b, float a) :
+			r(r),
+			g(g),
+			b(b),
+			a(a)
+		{
+		}
+		ColorRgba & ColorRgba::operator =(const ColorRgba &color)
+		{
+			r = color.r;
+			g = color.g;
+			b = color.b;
+			a = color.a;
+			return *this;
+		}
+		/* --------------------------------------------------------------------------- */
+		Vertex::Vertex()
+		{
+		}
+		Vertex::Vertex(const math::Vec3 position, const ColorRgba &color,
+			const math::Vec2 textureCoordinates) :
+			position(position),
+			color(color),
+			textureCoordinates(textureCoordinates)
+		{
+		}
+		Vertex::Vertex(const Vertex &vertex) :
+			position(vertex.position),
+			color(vertex.color),
+			textureCoordinates(vertex.textureCoordinates)
+		{
+		}
+		Vertex & Vertex::operator =(const Vertex &vertex)
+		{
+			position = vertex.position;
+			color = vertex.color;
+			textureCoordinates = vertex.textureCoordinates;
+			return *this;
+		}
 	}
 }
--- Source/Blitz_Graphics.h
+++ Source/Blitz_Graphics.h
@@ -8,6 +8,38 @@ namespace blitz
 {
 	namespace graphics
 	{
+		struct ColorRgb
+		{
+			float r, g, b;
+
+			ColorRgb();
+			ColorRgb(const ColorRgb &color);
+			ColorRgb(float r, float g, float b);
+			ColorRgb & operator =(const ColorRgb &color);
+		};
+
+		struct ColorRgba
+		{
+			float r, g, b, a;
+
+			ColorRgba();
+			ColorRgba(float r, float g, float b, float a);
+			ColorRgba(const ColorRgba &color);
+			ColorRgba & operator =(const ColorRgba &color);
+		};
+
+		struct Vertex
+		{
+			math::Vec3 position;
+			ColorRgba color;
+			math::Vec2 textureCoordinates;
+
+			Vertex();
+			Vertex(const math::Vec3 position, const ColorRgba &color,
+				const math::Vec2 textureCoordinates);
+			Vertex(const Vertex &vertex);
+			Vertex & operator =(const Vertex &vertex);
+		};
 	}
 }
 
