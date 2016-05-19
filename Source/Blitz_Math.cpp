137797f3917d7fc7e3c3d0b9af7d2f99d5b5004f
Sahand Malaei Balasi <sahandmalaei@gmail.com>
2016-05-20 02:56:03 +0430

    Removed the redundant empty lines in the default constructors of Vec2/Vec3.

--- Source/Blitz_Math.cpp
+++ Source/Blitz_Math.cpp
@@ -10,7 +10,6 @@ namespace blitz
 			x(0.0f),
 			y(0.0f)
 		{
-
 		}
 		Vec2::Vec2(const Vec2 &vector) :
 			x(vector.x),
@@ -132,7 +131,6 @@ namespace blitz
 			y(0.0f),
 			z(0.0f)
 		{
-
 		}
 		Vec3::Vec3(const Vec3 &vector) :
 			x(vector.x),
