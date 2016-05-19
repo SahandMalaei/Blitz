8a983b42059142825e808145b4285b449444e4da
Sahand Malaei Balasi <sahandmalaei@gmail.com>
2016-05-19 18:15:50 +0430

    Added files "Blitz_Graphics_Debug.h" and "Blitz_Graphics_Debug.cpp".

---
+++ Source/Blitz_Graphics_Debug.cpp
@@ -0,0 +1,11 @@
+#include "Blitz_Graphics_Debug.h"
+/* ----------------------------------------------------------------------------------- */
+namespace blitz
+{
+	namespace graphics
+	{
+		namespace debug
+		{
+		}
+	}
+}
---
+++ Source/Blitz_Graphics_Debug.h
@@ -0,0 +1,14 @@
+#ifndef BLITZ_GRAPHICS_DEBUG_H
+#define BLITZ_GRAPHICS_DEBUG_H 1
+
+namespace blitz
+{
+	namespace graphics
+	{
+		namespace debug
+		{
+		}
+	}
+}
+
+#endif
