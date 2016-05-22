72b3ea97c7fbe4fe3c2e3530a37adae2ac09ab18
Sahand Malaei Balasi <sahandmalaei@gmail.com>
2016-05-22 19:31:03 +0430

    Added files "Blitz_Graphics_Font.h" and "Blitz_Graphics_Font.cpp".

---
+++ Source/Blitz_Graphics_Font.cpp
@@ -0,0 +1,8 @@
+#include "Blitz_Graphics_Font.h"
+/* ------------------------------------------------------------------------------------ */
+namespace blitz
+{
+	namespace graphics
+	{
+	}
+}
---
+++ Source/Blitz_Graphics_Font.h
@@ -0,0 +1,13 @@
+#ifndef BLITZ_GRAPHICS_FONT_H
+#define BLITZ_GRAPHICS_FONT_H
+
+#include "Blitz_BaseTypes.h"
+
+namespace blitz
+{
+	namespace graphics
+	{
+	}
+}
+
+#endif
